8253d45447395cf5bb981439282dcf86642bf7ac
Fabiano <fabiano.ssantos79@gmail.com>
2022-04-15 01:50:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-/** help */
-function log(message) {
-    console.log('> ' + message)
-}
-
-/** app */
 const cards = document.querySelectorAll('.card')
 const dropzones = document.querySelectorAll('.dropzone')
 
-
-/** our cards */
 cards.forEach(card => {
     card.addEventListener('dragstart', dragstart)
     card.addEventListener('drag', drag)
@@ -16,27 +8,27 @@ cards.forEach(card => {
 })
 
 function dragstart() {
-    // log('CARD: Start dragging ')
-    dropzones.forEach( dropzone => dropzone.classList.add('highlight'))
+    //console.log('CARD: Start Dragging');
+    dropzones.forEach(dropzone => dropzone.classList.add('highlight'))
 
-    // this = card
     this.classList.add('is-dragging')
 }
 
 function drag() {
-    // log('CARD: Is dragging ')
+    //console.log('CARD: is Dragging');
+    
 }
 
 function dragend() {
-    // log('CARD: Stop drag! ')
-    dropzones.forEach( dropzone => dropzone.classList.remove('highlight'))
+    //console.log('Stoped Dragging');
+    dropzones.forEach(dropzone => dropzone.classList.remove('highlight'))
 
-    // this = card
     this.classList.remove('is-dragging')
 }
 
-/** place where we will drop cards */
-dropzones.forEach( dropzone => {
+
+
+dropzones.forEach(dropzone => {
     dropzone.addEventListener('dragenter', dragenter)
     dropzone.addEventListener('dragover', dragover)
     dropzone.addEventListener('dragleave', dragleave)
@@ -44,28 +36,25 @@ dropzones.forEach( dropzone => {
 })
 
 function dragenter() {
-    // log('DROPZONE: Enter in zone ')
+    console.log('DROPZONE: Enter in Zone');
 }
 
 function dragover() {
-    // this = dropzone
+    console.log('DROPZONE: Over');
+
     this.classList.add('over')
 
-    // get dragging card
-    const cardBeingDragged = document.querySelector('.is-dragging')
+    const cardDragged = document.querySelector('.is-dragging')
 
-    // this = dropzone
-    this.appendChild(cardBeingDragged)
+    this.appendChild(cardDragged)
 }
 
 function dragleave() {
-    // log('DROPZONE: Leave ')
-    // this = dropzone
-    this.classList.remove('over')
+    console.log('DROPZONE: Leave');
 
+    this.classList.remove('over')
 }
 
 function drop() {
-    // log('DROPZONE: dropped ')
-    this.classList.remove('over')
+    console.log('DROPZONE: Dropped');
 }
